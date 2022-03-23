76b1c36aedeea5bc774eab30ce4da3b29741a804
naseebpanghal1434 <naseebsingh3510@gmail.com>
2022-03-23 14:09:33 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function naseeb() {
+  console.log('hey this is naseeb');
+}
+
+naseeb();
