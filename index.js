9a4018ef45c151e671b4a98f1c4751ac7038e7b7
naseebpanghal1434 <naseebsingh3510@gmail.com>
2022-03-23 14:31:15 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,8 +5,40 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+//1. Simple function creation
 function naseeb() {
   console.log('hey this is naseeb');
 }
-
 naseeb();
+
+//2. function expression
+const naseebfun = function () {
+  console.log('this is my first functionc expression');
+};
+
+naseebfun();
+
+//3. arrow function
+const fname = () => {
+  console.log('This is arrow function');
+};
+fname();
+
+//4. Example of all practice exams
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sum(2, 2));
+
+const sum2 = function (num1, num2) {
+  return num1 + num2;
+};
+
+console.log(sum2(4, 4));
+
+const sum3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(sum3(8, 8));
